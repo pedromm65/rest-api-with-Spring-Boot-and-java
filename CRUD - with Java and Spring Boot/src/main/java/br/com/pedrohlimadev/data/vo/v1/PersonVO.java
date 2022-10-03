0a4b6d2870ec142f75ac5a9de786c16eee0dcd81
Pedro Lima <pedromm65@hotmail.com.br>
2022-10-03 15:02:54 -0300

    Custom Json Serialization

--- CRUD - with Java and Spring Boot/src/main/java/br/com/pedrohlimadev/data/vo/v1/PersonVO.java
+++ CRUD - with Java and Spring Boot/src/main/java/br/com/pedrohlimadev/data/vo/v1/PersonVO.java
@@ -1,18 +1,26 @@
 package br.com.pedrohlimadev.data.vo.v1;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonPropertyOrder;
+
 import java.io.Serializable;
 import java.util.Objects;
 
+@JsonPropertyOrder({"id", "firstName", "lastName", "address", "gender"})
 public class PersonVO implements Serializable {
 
     private Long id;
 
+    @JsonProperty("first_name")
     private String firstName;
 
+    @JsonProperty("last_name")
     private String lastName;
 
     private String address;
 
+    @JsonIgnore
     private String gender;
 
     public PersonVO() {}
