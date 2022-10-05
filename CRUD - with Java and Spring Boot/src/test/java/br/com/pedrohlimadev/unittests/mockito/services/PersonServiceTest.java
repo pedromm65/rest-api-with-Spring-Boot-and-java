db7d490ab14f2652e16d3b35b32b63142fd25fcb
Pedro Lima <pedromm65@hotmail.com.br>
2022-10-05 15:07:40 -0300

    ERROR NO TESTE DE CRIAÇÃO

---
+++ CRUD - with Java and Spring Boot/src/test/java/br/com/pedrohlimadev/unittests/mockito/services/PersonServiceTest.java
@@ -0,0 +1,103 @@
+package br.com.pedrohlimadev.unittests.mockito.services;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
+
+import br.com.pedrohlimadev.data.vo.v1.PersonVO;
+import br.com.pedrohlimadev.model.Person;
+import br.com.pedrohlimadev.repositories.PersonRepository;
+import br.com.pedrohlimadev.services.PersonService;
+import br.com.pedrohlimadev.unittests.mapper.mocks.MockPerson;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInstance;
+import org.junit.jupiter.api.TestInstance.Lifecycle;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.util.Optional;
+
+
+@TestInstance(Lifecycle.PER_CLASS)
+@ExtendWith(MockitoExtension.class)
+class PersonServiceTest {
+
+    MockPerson input;
+
+    @InjectMocks
+    private PersonService service;
+
+    @Mock
+    PersonRepository repository;
+
+    @BeforeEach
+    void SetUpMocks() throws Exception {
+        input = new MockPerson();
+        MockitoAnnotations.openMocks(this);
+    }
+
+    @Test
+    void findAll() {
+    }
+
+    @Test
+    void findById() {
+        Person entity = input.mockEntity();
+        entity.setId(1L);
+
+        when(repository.findById(1L)).thenReturn(Optional.of(entity));
+
+        var result = service.findById(1L);
+
+        assertNotNull(result);
+        assertNotNull(result.getKey());
+        assertNotNull(result.getLinks());
+
+        assertTrue(result.toString().contains("links: [</api/person/v1/1>;rel=\"self\"]"));
+        assertEquals("Address Test0", result.getAddress());
+        assertEquals("First Name Test0", result.getFirstName());
+        assertEquals("Last Name Test0", result.getLastName());
+        assertEquals("Male", result.getGender());
+
+    }
+
+    @Test
+    void create() {
+        Person entity = input.mockEntity(1);
+        entity.setId(1L);
+
+        Person persisted = entity;
+        persisted.setId(1L);
+
+        PersonVO vo = input.mockVO(1);
+        vo.setKey(1L);
+
+        when(repository.save(entity)).thenReturn(persisted);
+
+
+        //ERRORRRR
+        var result = service.create(vo);
+
+        assertNotNull(result);
+        assertNotNull(result.getKey());
+        assertNotNull(result.getLinks());
+
+        assertTrue(result.toString().contains("links: [</api/person/v1/1>;rel=\"self\"]"));
+        assertEquals("Address Test1", result.getAddress());
+        assertEquals("First Name Test1", result.getFirstName());
+        assertEquals("Last Name Test1", result.getLastName());
+        assertEquals("Female", result.getGender());
+    }
+
+    @Test
+    void update() {
+    }
+
+    @Test
+    void delete() {
+    }
+}
